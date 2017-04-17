f80499feed818eabae7db6ce92df54b7423b5041
Marques Jordan <marques.jordan81@gmail.com>
2017-04-16 23:15:35 -0700

    Add Functions For Beta Section

--- imports/ui/components/Beta.jsx
+++ imports/ui/components/Beta.jsx
@@ -6,7 +6,38 @@ import Thanks from './Thanks.jsx';
 
 // Task component - represents a single todo item
 export default class Beta extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleJoinClick = this.handleJoinClick.bind(this);
+    this.handleCorpClick = this.handleCorpClick.bind(this);
+    this.state = {
+      join: false,
+      corp: false
+    };
+  }
+
+  handleJoinClick() {
+    this.setState({join: true, corp: false});
+  }
+
+  handleCorpClick() {
+    this.setState({corp: true, join: false});
+  }
+
   render() {
+    const isJoin = this.state.join;
+    const isCorp = this.state.corp;
+
+    let myForm = null;
+    if (isJoin) {
+      myForm = <Join />;
+    }
+
+    if (isCorp) {
+      myForm = <Corporate />;
+    }
+
     return (
       <div className="beta-section">
       	<div className="container">
@@ -21,10 +52,10 @@ export default class Beta extends Component {
                 <div className="pad-bottom-lg">
                   <div className="btn-group btn-group-justified" role="group" aria-label="...">
                     <div className="btn-group" role="group">
-                      <button type="button" className="btn btn-primary">Join The Group</button>
+                      <button type="button" className="btn btn-primary" onClick={this.handleJoinClick}>Join The Group</button>
                     </div>
                     <div className="btn-group" role="group">
-                      <button type="button" className="btn btn-default">Corporate</button>
+                      <button type="button" className="btn btn-default" onClick={this.handleCorpClick}>Corporate</button>
                     </div>
                   </div>
                 </div>
@@ -34,9 +65,7 @@ export default class Beta extends Component {
               </div>
             </div>
 
-            <Join></Join>
-            <Thanks></Thanks>
-            <Corporate></Corporate>
+            {myForm}
 
       		</div>
       	</div>
