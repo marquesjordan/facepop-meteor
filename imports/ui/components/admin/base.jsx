992aa2d7bc63efbd78c082362cda25795362f456
Marques Jordan <marques.jordan81@gmail.com>
2017-05-24 20:34:41 -0700

    Set Jquery to correct version

--- imports/ui/components/admin/base.jsx
+++ imports/ui/components/admin/base.jsx
@@ -37,43 +37,52 @@ export default class Base extends Component {
         <NavBar />
       	<div className="container">
       		<div className="row">
+
             <div className="col-sm-8 col-sm-offset-2">
+
               <div className="jumbotron text-center">
                 <h1 className="text-center">Admin</h1>
                 <button className="btn btn-primary btn-lg" onClick={this.handleShowStats}>Show Stats</button>
                 <p className="bg-danger">{this.state.errorMessage}</p>
-            </div>
+              </div>
 
               {this.state.isAdmin &&
-                <div id="exTab2" className="">
-                  <ul className="nav nav-tabs">
-                  	<li className="active">
-                      <a  href="#1" data-toggle="tab">VIP Access</a>
-                  	</li>
-                  	<li><a href="#2" data-toggle="tab">VIP Log</a>
-                  	</li>
-                  	<li><a href="#3" data-toggle="tab">Survey Results</a>
-                  	</li>
-                  </ul>
+                <div className="panel with-nav-tabs panel-default">
 
-                	<div className="tab-content ">
-                	  <div className="tab-pane active" id="1">
-                      <VipAccess />
-                      <VipAccessList />
-                		</div>
-                		<div className="tab-pane" id="2">
-                      <VipGuest />
-                		</div>
-                    <div className="tab-pane" id="3">
-ddd
+                  <div id="exTab2" className="panel-heading">
+                    <ul className="nav nav-tabs">
+                    	<li className="active">
+                        <a  href="#1a" data-toggle="tab">VIP Access</a>
+                    	</li>
+                    	<li><a href="#2a" data-toggle="tab">VIP Log</a>
+                    	</li>
+                    	<li><a href="#3a" data-toggle="tab">Survey Results</a>
+                    	</li>
+                    </ul>
+                  </div>
+                  <div className="panel-body">
+                    <div className="tab-content clearfix">
+                      <div className="tab-pane active" id="1a">
+                        <VipAccess />
+                        <VipAccessList />
+                      </div>
+                      <div className="tab-pane" id="2a">
+                        <VipGuest />
+                      </div>
+                      <div className="tab-pane" id="3a">
+                        ddd
+                      </div>
                     </div>
-                	</div>
+                  </div>
+
                 </div>
+
               }
             </div>
-      		</div>
-      	</div>
-      </div>
+
+          </div>
+    		</div>
+    	</div>
     );
   }
 }
