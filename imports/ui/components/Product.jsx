feb373e73def1cdbf97535ec1bfcdf7c134ddd6d
Marques Jordan <marques.jordan81@gmail.com>
2017-06-07 22:15:29 -0700

    Fix Contact Video top

--- imports/ui/components/Product.jsx
+++ imports/ui/components/Product.jsx
@@ -9,7 +9,7 @@ export default class Product extends Component {
   }
 
   render() {
-    const product1Style = {top: '-73px', left: '-83px'};
+    const product1Style = {top: '-75px', left: '-83px'};
     const product2Style = {top: '-76px', left: '-75px'};
     const product3Style = {top: '-76px', left: '-65px'};
     const product4Style = {top: '-75px', left: '-70px'};
