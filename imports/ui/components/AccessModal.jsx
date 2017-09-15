6d485a41cbb1e1f2193aefa351d7f7e9a9ea8fe3
Marques Jordan <marques.jordan81@gmail.com>
2017-09-14 21:12:24 -0700

    Add checkbox to vip form

--- imports/ui/components/AccessModal.jsx
+++ imports/ui/components/AccessModal.jsx
@@ -15,7 +15,7 @@ class AccessModal extends Component {
       errorMessage: '',
       ip: '',
       isInvestor: false
-    }
+    };
 
     this.checkInvestor = this.checkInvestor.bind(this);
   }
@@ -25,17 +25,16 @@ class AccessModal extends Component {
   }
 
   UserIP() {
-    $.getJSON('http://ipinfo.io')
-      .then((data) => {
-        this.setState({ip: data.ip});
-      });
+    $.getJSON('http://ipinfo.io').then(data => {
+      this.setState({ ip: data.ip });
+    });
   }
 
   checkInvestor(email) {
-    var emailItem = this.props.investors.filter( (item) => {
+    var emailItem = this.props.investors.filter(item => {
       return item.email === email;
-    })
-    if(emailItem.length > 0) {
+    });
+    if (emailItem.length > 0) {
       return true;
     }
 
@@ -43,74 +42,97 @@ class AccessModal extends Component {
   }
 
   handleSubmit(event) {
-    const ALL_ACCESS_CODE = "100billion";
-
+    const ALL_ACCESS_CODE = '100billion';
     event.preventDefault();
-    const allAcessCode = ReactDOM.findDOMNode(this.refs.allAcessCode).value.trim();
+    const allAcessCode = ReactDOM.findDOMNode(
+      this.refs.allAcessCode
+    ).value.trim();
     const email = ReactDOM.findDOMNode(this.refs.email).value.trim();
+    const checkbox = ReactDOM.findDOMNode(this.refs.checkbox).checked;
 
-    if(allAcessCode === ALL_ACCESS_CODE && this.checkInvestor(email)) {
+    if (!checkbox) {
+      this.setState({
+        errorMessage: 'Must click checkbox and agree to terms.'
+      });
+    } else if (allAcessCode === ALL_ACCESS_CODE && this.checkInvestor(email)) {
       const ip = this.state.ip;
 
       Vips.insert({
         email,
         ip,
-        createdAt: new Date(), // current time
+        createdAt: new Date() // current time
       });
 
-      this.setState({granted: true, errorMessage: ''});
+      this.setState({ granted: true, errorMessage: '' });
 
       this.props.handleModalSubmit(false);
-
     } else {
-      this.setState({errorMessage: 'Wrong Password'});
-
+      this.setState({ errorMessage: 'Wrong Password' });
     }
   }
 
   renderFooter() {
-    if(this.state.granted) {
+    if (this.state.granted) {
       return (
         <div className="text-center">
           <h1>ACCESS GRANTED</h1>
         </div>
-      )
+      );
     }
 
     return (
       <div>
         <div className="row">
           <div className="col-md-10 col-md-offset-1">
-            <div className="text-left pad-bottom-lg">
-              By clicking on the "Submit" button you are agreeing to the terms provided herein.
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6 col-md-offset-3">
             <form className="beta-form" onSubmit={this.handleSubmit.bind(this)}>
+              <div className="form-check">
+                <label className="form-check-label">
+                  <input
+                    type="checkbox"
+                    className="form-check-input checkbox-vip"
+                    id="checkbox"
+                    ref="checkbox"
+                  />
+                  By selecting the checkbox, you are agreeing to the terms
+                  provided herein.
+                </label>
+              </div>
               <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" id="email" ref="email" placeholder="Enter Your Email Address" />
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  ref="email"
+                  placeholder="Enter Your Email Address"
+                />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" ref="allAcessCode" id="password" placeholder="Password" />
+                <input
+                  type="password"
+                  className="form-control"
+                  ref="allAcessCode"
+                  id="password"
+                  placeholder="Password"
+                />
                 <div className="error-message">
                   {this.state.errorMessage}
                 </div>
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   render() {
-    const modalClassName = classNames( this.props.className, {
-      'modal': true,
+    const modalClassName = classNames(this.props.className, {
+      modal: true,
       'show-modal': this.props.showModal
     });
 
@@ -122,42 +144,79 @@ class AccessModal extends Component {
               <button className="close" onClick={this.props.onCancel}>
                 <span aria-hidden="true">&times;</span>
               </button>
-              <h4 className="modal-title">{this.props.title}</h4>
+              <h4 className="modal-title">
+                {this.props.title}
+              </h4>
             </div>
             <div className="modal-body">
               <div className="modal-body-container">
                 <p>DISCLAIMER NOTICE</p>
-                <p><br /></p>
+                <p>
+                  <br />
+                </p>
                 <p>USPTO PATENT PENDING FILE SERIAL No. 15/355,038</p>
                 <p>THE FACEPOP COMPANY, LLC.&nbsp;</p>
-                <p>The information contained across this section of the website
-                  is confidential and proprietary to</p>
-                <p>Bervick J. Deculus II, and The Facepop Company, Inc.
-                  including potential employees, principals, family members,
-                  associates, subsidiaries, successors, and or assigns
-                  (collectively referred to as “App Company”) and is intended
-                  only for the persons to whom it is transmitted by the App
-                  Company or its representatives. Any reproduction of this
-                  document, in whole or in part, or the divulgence of any of
-                  its contents without the prior written consent of App Company,
-                  is prohibited.</p>
-                <p>The App Company filed a patent pending status and it does
-                  not imply and shall not be construed as an offering of
-                  securities. Prospective investors are not to construe the
-                  contents of this document as investment, legal or tax advice
-                  from either App Company or those that prepared this document.
-                  Any prospective investor should consult with professional
-                  investment advisors and gain professional legal and tax
-                  advice.</p>
-                <p>Each potential investor, consultant, independent contractor and/or employee specifically understands and agrees that any estimates, projections, revenue models, forecasts or assumptions are by definition uncertain and thus possibly unreliable. Any party considering a transaction with App Company agrees to look solely to its own diligence.</p>
+                <p>
+                  The information contained across this section of the website
+                  is confidential and proprietary to
+                </p>
+                <p>
+                  Bervick J. Deculus II, and The Facepop Company, Inc. including
+                  potential employees, principals, family members, associates,
+                  subsidiaries, successors, and or assigns (collectively
+                  referred to as “App Company”) and is intended only for the
+                  persons to whom it is transmitted by the App Company or its
+                  representatives. Any reproduction of this document, in whole
+                  or in part, or the divulgence of any of its contents without
+                  the prior written consent of App Company, is prohibited.
+                </p>
+                <p>
+                  The App Company filed a patent pending status and it does not
+                  imply and shall not be construed as an offering of securities.
+                  Prospective investors are not to construe the contents of this
+                  document as investment, legal or tax advice from either App
+                  Company or those that prepared this document. Any prospective
+                  investor should consult with professional investment advisors
+                  and gain professional legal and tax advice.
+                </p>
+                <p>
+                  Each potential investor, consultant, independent contractor
+                  and/or employee specifically understands and agrees that any
+                  estimates, projections, revenue models, forecasts or
+                  assumptions are by definition uncertain and thus possibly
+                  unreliable. Any party considering a transaction with App
+                  Company agrees to look solely to its own diligence.
+                </p>
 
-                <p>Confidentiality Agreement: The recipient of this document acknowledges that the information provided by</p>
-                <p>App Company in this website is confidential. Therefore, the recipient of this document agrees not to disclose any of such information without the express written permission of App Company. Additionally; as said information is to be treated as confidential; any non-disclosure, confidentiality agreement, non-circumvention and/or other similar agreement by and between App Company and the recipient of this business plan shall be binding upon the parties with respect to the information contained in this website.</p>
+                <p>
+                  Confidentiality Agreement: The recipient of this document
+                  acknowledges that the information provided by
+                </p>
+                <p>
+                  App Company in this website is confidential. Therefore, the
+                  recipient of this document agrees not to disclose any of such
+                  information without the express written permission of App
+                  Company. Additionally; as said information is to be treated as
+                  confidential; any non-disclosure, confidentiality agreement,
+                  non-circumvention and/or other similar agreement by and
+                  between App Company and the recipient of this business plan
+                  shall be binding upon the parties with respect to the
+                  information contained in this website.
+                </p>
 
-                <p>In receipt of the App Company's "All Access" invite, it is hereby acknowledge by the invitee that the information furnished in this website is in all respects confidential in nature (other than such information which is already in the public domain through other means) and that any disclosure or use of same by the undersigned may cause serious harm or damage to App Company.</p>
-                <p><br /></p>
+                <p>
+                  In receipt of the App Company's "All Access" invite, it is
+                  hereby acknowledge by the invitee that the information
+                  furnished in this website is in all respects confidential in
+                  nature (other than such information which is already in the
+                  public domain through other means) and that any disclosure or
+                  use of same by the undersigned may cause serious harm or
+                  damage to App Company.
+                </p>
+                <p>
+                  <br />
+                </p>
               </div>
-
             </div>
             <div className="modal-footer">
               {this.renderFooter()}
@@ -174,5 +233,5 @@ export default createContainer(() => {
 
   return {
     investors: Investors.find({}).fetch()
-  }
+  };
 }, AccessModal);
